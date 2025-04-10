44ac26dddf6f603438f7fbc7c191de139f330954
KaSSiniOne <121349562+KaSSiniOne@users.noreply.github.com>
2025-04-10 19:55:40 +1000

    Убраны мусорные папки, изменена структуа проекта

---
+++ Web_dev/frontend/tamplates/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WeWe - вместе интереснее</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="MeWe/Web_dev/frontend/static/css/styles.css" rel="stylesheet">
+</head>
+<body>
+    <header class="top-nav">
+        <div class="tab active">пример 1</div>
+        <div class="tab">пример 1</div>
+        <div class="tab">пример 1</div>
+    </header>
+    <div class="content">
+    </div>
+    <footer class="bottom-nav">
+        <div class="nav-icon">🔍</div>
+        <div class="nav-icon">📅</div>
+        <div class="nav-icon">🕒</div>
+    </footer>
+</body>
+</html>
