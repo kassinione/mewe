9163b91e93cb0126c33a1505809c130273574a66
KaSSiniOne <121349562+KaSSiniOne@users.noreply.github.com>
2025-04-16 21:51:24 +1000

    Добавлена папка Amvera для деплоя тестового приложения на бесплатный хостинг

--- Amvera/index.php
+++ Amvera/index.php
@@ -5,20 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WeWe - вместе интереснее</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <link href="MeWe/Web_dev/frontend/static/css/styles.css" rel="stylesheet">
+    <link href="{{ url_for('static', filename='css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <header class="top-nav">
-        <div class="tab active">пример 1</div>
+        <div class="tab">пример 1</div>
+        <div class="tab">пример 1</div>
         <div class="tab">пример 1</div>
         <div class="tab">пример 1</div>
     </header>
     <div class="content">
+        <div class="content-block"></div>
+        <div class="content-block"></div>
+        <div class="content-block"></div>
+        <div class="content-block"></div>
+        <div class="content-block"></div>
+        <div class="content-block"></div>
     </div>
     <footer class="bottom-nav">
         <div class="nav-icon">🔍</div>
         <div class="nav-icon">📅</div>
         <div class="nav-icon">🕒</div>
     </footer>
+    <script src="{{ url_for('static',filename='js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/scripts.js') }}"></script>
 </body>
 </html>
