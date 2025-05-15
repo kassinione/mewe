6958775ff3751250c71e624d409f30f027beb533
KaSSiniOne <121349562+KaSSiniOne@users.noreply.github.com>
2025-05-15 19:47:27 +1000

    Выделил файлы для веб разработки фронтенда, изменил фалы приложения

---
+++ Web-dev/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($title) ? $title : 'MeWe'; ?></title>
+    <link href="static/css/main.css" rel="stylesheet">
+    <link href="static/css/footer.css" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <div class="main">
+        <img id="logo-png" src="static/icons/bot.png" alt="bot_png">
+    </div>
+    <?php include 'footer.html'; ?>
+</div>
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
+</body>
+</html>
