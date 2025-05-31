384f90fa3785df5ddf6f0188379fc732bc785b3c
kollol167 <kollol167@gmail.com>
2025-05-31 23:10:16 +1000

    строка поиска

--- Web-dev/index.php
+++ Web-dev/index.php
@@ -10,10 +10,29 @@
 <body>
 <div class="wrapper">
     <div class="main">
+        <div class="search-bar-container">
+            <input type="text" class="search-input" id="searchInput" placeholder="Поиск...">
+            <img src="static/icons/search.png" alt="Поиск" class="search-icon" id="searchIcon">
+        </div>
         <img id="logo-png" src="static/icons/logo.png" alt="logo_png">
     </div>
     <?php include 'partials/footer.html'; ?>
 </div>
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
+<script>
+    const searchInput = document.getElementById('searchInput');
+    const searchIcon = document.getElementById('searchIcon');
+
+    searchInput.addEventListener('focus', () => {
+        searchIcon.classList.add('active');
+    });
+    searchInput.addEventListener('blur', () => {
+        searchIcon.classList.remove('active');
+    });
+    searchIcon.addEventListener('click', () => {
+        searchInput.focus();
+        searchIcon.classList.add('active');
+    });
+</script>
 </body>
 </html>
