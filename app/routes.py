5a12acca617dafdd3ffdb0d5af06bd8f48f962ee
kassinione <feelinglev@gmail.com>
2026-05-01 13:10:14 +1000

    chore: clean up repository and update .gitignore

--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-main = Blueprint("main", __name__)  # имя и модуль
-
-@main.route("/")
-def index():
-    return render_template("index.html", title="mewe")
