173cacef1172738f20940ced4b5d1accd84f6264
kassinione <feelinglev@gmail.com>
2026-09-09 16:57:34 +1000

    fix:secret key init in app function

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -13,6 +13,8 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
     app.json.ensure_ascii = False  # type: ignore
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
